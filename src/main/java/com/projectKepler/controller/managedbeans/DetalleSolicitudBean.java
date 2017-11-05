1df82b4a457cd5e76774462e3779ad094af71459
Julian Mateo Guzman Diaz <matguz96@hotmail.com>
2017-11-04 22:46:37 -0500

    m.guzman vista detalleSol

--- src/main/java/com/projectKepler/controller/managedbeans/DetalleSolicitudBean.java
+++ src/main/java/com/projectKepler/controller/managedbeans/DetalleSolicitudBean.java
@@ -6,6 +6,7 @@
 package com.projectKepler.controller.managedbeans;
 
 
+import java.util.Date;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -17,10 +18,50 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class DetalleSolicitudBean{
 
-    /**
-     * Creates a new instance of DetalleSolicitud
-     */
     public DetalleSolicitudBean() {
+        this.fecha = new Date();
+        this.profesor = "Hector cadavid";
+        this.codigo = 2103110;
+        this.estudiante = "Diego Borrero";
+    }
+    
+    public Date fecha;
+    public String profesor;
+    public int codigo;
+    public String estudiante;
+
+    public String getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(String estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = new Date();
     }
     
 }
