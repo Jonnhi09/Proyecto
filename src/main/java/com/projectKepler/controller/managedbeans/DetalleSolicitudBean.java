945781fd4d28984d2011bd2ee6ca5ad46306dc35
Diego Borrero <die.9511@gmail.com>
2017-11-04 19:53:38 -0500

    (d.borrero)(m.guzman) views first commit

---
+++ src/main/java/com/projectKepler/controller/managedbeans/DetalleSolicitudBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projectKepler.controller.managedbeans;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author dxmortem
+ */
+@ManagedBean(name = "detalleSolicitud")
+@SessionScoped
+public class DetalleSolicitudBean{
+
+    /**
+     * Creates a new instance of DetalleSolicitud
+     */
+    public DetalleSolicitudBean() {
+    }
+    
+}
