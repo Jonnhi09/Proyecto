f26a311d56c207e020ee612f2c0c1a2c7502fd74
Diego Borrero <die.9511@gmail.com>
2017-11-07 01:38:00 -0500

    (d.borrero) vista SolCancel

--- src/main/java/com/projectKepler/controller/managedbeans/SolCancelBean.java
+++ src/main/java/com/projectKepler/controller/managedbeans/SolCancelBean.java
@@ -5,6 +5,8 @@
  */
 package com.projectKepler.controller.managedbeans;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -15,11 +17,67 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "solCancelBean")
 @SessionScoped
 public class SolCancelBean {
+    List<String> materia;
+    String estudiante;
+    String materiaSeleccionada;
+    boolean panelVisibility;
+    
+    
+    public String getMateriaSeleccionada() {
+        return materiaSeleccionada;
+    }
 
+    public void setMateriaSeleccionada(String materiaSeleccionada) {
+        this.materiaSeleccionada = materiaSeleccionada;
+    }
+    
+    public List<String> getMateria() {
+        return materia;
+    }
+
+    public void setMateria(List<String> materia) {
+        this.materia = materia;
+    }
+
+    public String getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(String estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public boolean isPanelVisibility() {
+        return panelVisibility;
+    }
+
+    public void setPanelVisibility(boolean panelVisibility) {
+        this.panelVisibility = panelVisibility;
+    }
+    
     /**
      * Creates a new instance of SolCancelBean
      */
     public SolCancelBean() {
+        this.panelVisibility = false;
+        this.materia = new ArrayList<>();
+        for(int i = 0; i<10; i++){
+            materia.add("Materia " + i);
+        }
+        estudiante = "Pepito";
+    }
+    
+    public void makePanelVisible(){
+        setPanelVisibility(true);
+    }
+    
+    public void makePanelInvisible(){
+        setPanelVisibility(false);
+    }
+    
+    public void analisis(){
+        //algo
+        makePanelVisible();
     }
     
 }
